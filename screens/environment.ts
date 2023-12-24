42a622671893e9008098208732e81ad0fbb046b7
KayZinHtetNaing <nnoe06915@gmail.com>
2023-12-24 02:33:25 -0800

    mapUpdate

---
+++ screens/environment.ts
@@ -0,0 +1,1 @@
+export const GOOGLE_API_KEY = "AIzaSyBEy4koPCAzTlCtF2vSDFiDZlEdKfj-5ME";
--- screens/home.jsx
+++ screens/home.jsx
@@ -1,10 +1,106 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React from 'react';
+import MapView, { PROVIDER_GOOGLE} from 'react-native-maps';
+import { StyleSheet, View,Text, Dimensions} from 'react-native';
+// import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete';
+import Constants from 'expo-constants';
+import {
+  GooglePlaceDetail,
+  GooglePlacesAutocomplete,
+} from "react-native-google-places-autocomplete";
+import { GOOGLE_API_KEY } from './environment';
+//18.818752452008397, 95.220778226511
+const { width, height } = Dimensions.get("window");
+
+const ASPECT_RATIO = width / height;
+const LATITUDE_DELTA = 0.02;
+const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+const INITIAL_POSITION = {
+  latitude: 18.818752452008397,
+  longitude: 95.220778226511,
+  latitudeDelta: LATITUDE_DELTA,
+  longitudeDelta: LONGITUDE_DELTA,
+};
+
+type InputAutocompleteProps = {
+  label: string;
+  placeholder?: string;
+  onPlaceSelected: (details: GooglePlaceDetail | null) => void;
+};
+
+function InputAutocomplete({
+  label,
+  placeholder,
+  onPlaceSelected,
+}: InputAutocompleteProps) {
+  return (
+    <>
+      <Text>{label}</Text>
+      <GooglePlacesAutocomplete
+        styles={{ textInput: styles.input }}
+        placeholder={placeholder || ""}
+        fetchDetails
+        onPress={(data, details = null) => {
+          onPlaceSelected(details);
+        }}
+        query={{
+          key: GOOGLE_API_KEY,
+          language: "pt-BR",
+        }}
+      />
+    </>
+  );
+}
 
 export default function HomeScreen() {
     return ( 
-        <View style={{flex:1 , alignItems:"center",justifyContent:"center"}}>
-            <Text>HomeScreen</Text>
+        <View style={styles.container}>
+        <MapView style={styles.map} provider={PROVIDER_GOOGLE} initialRegion={INITIAL_POSITION}/>
+        <View style={styles.searchcontainer}>
+        <InputAutocomplete
+          label="Origin"
+          onPlaceSelected={(details) => {
+            onPlaceSelected(details, "origin");
+          }}
+        />
+        <InputAutocomplete
+          label="Destination"
+          onPlaceSelected={(details) => {
+            onPlaceSelected(details, "destination");
+          }}
+        />
         </View>
+      </View>
+      
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#fff",
+      alignItems: "center",
+      justifyContent: "center",
+    },
+    map: {
+      width: Dimensions.get("window").width,
+      height: Dimensions.get("window").height,
+    },
+    searchcontainer:{
+      position:"absolute",
+      width:"90%",
+      backgroundColor:"white",
+      shadowColor:"black",
+      shadowOffset:{width:2,height:2},
+      shadowOpacity:0.5,
+      shadowRadius:4,
+      elevation:4,
+      padding:10,
+      borderRadius:8,
+      top:Constants.statusBarHeight,
+    },
+    input:{
+     borderColor:"grey", 
+     borderWidth:1,
+     margin:4,
+    }
+  });
